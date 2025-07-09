647884e798f9d9e8132bab90026d5b11211137e7
GabrielRioo <gabrielpaivario@gmail.com>
2025-07-08 22:41:05 -0300

    criando as rotas de sala e criar sala + estrutura basica

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,17 +11,17 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts';
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173', 
+  origin: 'http://localhost:5173',
 });
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.get('/health', () => {
-  return 'OK'
-})
+  return 'OK';
+});
 
-app.register(getRoomsRoute)
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!');
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,8 +2,9 @@ import './index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { App } from './app'
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory by react
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
