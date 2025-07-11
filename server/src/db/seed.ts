e840615230099d78b5f79b74b1bd4eee5259f6fd
GabrielRioo <gabrielpaivario@gmail.com>
2025-07-10 22:11:19 -0300

    criando o front para criar e listar salas

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -7,14 +7,14 @@ await reset(db, schema);
 await seed(db, schema).refine((faker) => {
   return {
     rooms: {
-      count: 20,
+      count: 5,
       columns: {
         name: faker.companyName(),
         description: faker.loremIpsum(),
       },
     },
     questions: {
-      count: 50,
+      count: 20,
     },
   };
 });
